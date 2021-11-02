79010887aa1b9dcd4b90aedcf5e1038094093a70
Rida Dzhaafar <rdzhaafar@gmail.com>
2021-11-02 20:44:26 +0300

    Removed the package manager from init.el. So long.

--- init.el
+++ init.el
@@ -1,109 +1,56 @@
 ;; -*- lexical-binding: t -*-
 
+;; Disable autosave files and all kinds of miscellaneous
+;; garbage that litters in source directories
 (setq-default custom-file
 	      (expand-file-name ".custom.el" user-emacs-directory))
 (when
     (file-exists-p custom-file)
   (load custom-file))
-
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
-
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
-
 (setq create-lockfiles nil)
 
+;; Alias "yes or no" prompt to "y or n" because I can't
+;; be bothered to type 2 extra letters
 (defalias 'yes-or-no-p 'y-or-n-p)
 
-(require 'package)
-(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
-
-(when (not (package-installed-p 'use-package))
-  (package-refresh-contents)
- (package-install 'use-package))
-
-(require 'use-package-ensure)
-(setq use-package-always-ensure t)
-(use-package auto-compile
-  :config
-  (auto-compile-on-load-mode))
-(setq load-prefer-newer t)
-
-
-(defun font-installed-p (font)
-  "check if a font is installed"
-  (let ((f (find-font (font-spec :name font))))
+;; Set default font to SF Mono if installed
+(defun font-is-installed (font-name)
+  "check if font is installed on a system"
+  (let ((f (find-font (font-spec :name font-name))))
     (if (null f)
 	nil
       t)))
 
-(set-face-attribute 'default nil
-                    :family
-		    (if (font-installed-p "JetBrains Mono NL")
-			"JetBrains Mono NL"
-		      "Source Code Pro")
-                    :height 120
-                    :weight 'normal
-                    :width 'normal)
-
+(if (font-is-installed "SF Mono")
+    (set-face-attribute 'default nil
+			:family "SF Mono"
+			:height 130
+			:weight 'normal
+			:width 'normal))
+			
+;; Visual tweaks
 (menu-bar-mode -1)
+(blink-cursor-mode -1)
+(global-linum-mode 1)
 (when (window-system)
   (progn
     (setq frame-resize-pixelwise t)
     (toggle-scroll-bar -1)
-    (tool-bar-mode -1)))
+    (tool-bar-mode -1)
+    (global-hl-line-mode 1)
+    (set-frame-size (selected-frame) 110 70)))
 
-(add-hook 'prog-mode-hook 'linum-mode)
-
-(blink-cursor-mode -1)
-
+;; Disable startup screen and turn off the GNU
+;; welcome message
+(defun no-startup-echo-area-message ()
+  (message ""))
+(defalias 'startup-echo-area-message 'no-startup-echo-area-message)
 (setq inhibit-startup-screen t)
 
-(defun rd-startup-echo-area-message ()
-  (message ""))
-(defalias 'startup-echo-area-message 'rd-startup-echo-area-message)
-
-(use-package doom-themes
-  :config
-  (setq doom-themes-enable-bold t
-	doom-themes-enable-italic t)
-  (load-theme 'doom-opera))
-	
-
-(use-package ivy
-  :diminish
-  :custom
-  (ivy-use-virtual-buffers t)
-  (ivy-use-selectable-prompt t)
-  :config
-  (ivy-mode))
-
-(use-package counsel
-  :after
-  (ivy)
-  :config
-  (counsel-mode))
-
-(use-package swiper
-  :after
-  (ivy)
-  :bind
-  (("C-s" . swiper)
-   ("C-r" . swiper)))
-
-; If running on Windows (which is not likely, but still happens)
-; Set the home to "~" instead of AppData
-(if (string-equal system-type "windows-nt")
-    (setq default-directory "C:/Users/Rida/"))
-
-; Fix default C/C++ formatting
+;; Fix default C/C++ formatting
 (setq c-default-style "linux"
       c-basic-offset 4)
-
-; NOTE: GCMH should always be loaded last
-(use-package gcmh
-	     :init
-	     (gcmh-mode 1))
