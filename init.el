133d2b0eb5619e622a199562edc236002183f9ee
Rida Dzhaafar <rdzhaafar@gmail.com>
2021-11-09 16:27:06 +0300

    Added go-mode

--- init.el
+++ init.el
@@ -1,5 +1,9 @@
 ;; -*- lexical-binding: t -*-
 
+;; ==================================================================
+;; Random tweaks
+;; ==================================================================
+
 ;; Disable autosave files and all kinds of miscellaneous
 ;; garbage that litters in source directories
 (setq-default custom-file
@@ -17,7 +21,10 @@
 ;; be bothered to type 2 extra letters
 (defalias 'yes-or-no-p 'y-or-n-p)
 
-;; Set default font to SF Mono if installed
+;; ==================================================================
+;; Visual tweaks
+;; ==================================================================
+
 (defun font-is-installed (font-name)
   "check if font is installed on a system"
   (let ((f (find-font (font-spec :name font-name))))
@@ -25,17 +32,25 @@
 	nil
       t)))
 
+;; Change default font to SF Mono if installed
 (if (font-is-installed "SF Mono")
     (set-face-attribute 'default nil
 			:family "SF Mono"
 			:height 130
 			:weight 'normal
 			:width 'normal))
-			
-;; Visual tweaks
+
+;; Disable menu bar and blinking cursor
 (menu-bar-mode -1)
 (blink-cursor-mode -1)
+
+;; Show line numbers everywhere
 (global-linum-mode 1)
+
+;; Change cursor style to '|'
+(setq-default cursor-type 'bar)
+
+;; Visual settings when running in GUI mode
 (when (window-system)
   (progn
     (setq frame-resize-pixelwise t)
@@ -44,13 +59,25 @@
     (global-hl-line-mode 1)
     (set-frame-size (selected-frame) 110 70)))
 
-;; Disable startup screen and turn off the GNU
-;; welcome message
+;; Hide startup screen and inhibit the welcome message
+(setq inhibit-startup-screen t)
+
 (defun no-startup-echo-area-message ()
   (message ""))
 (defalias 'startup-echo-area-message 'no-startup-echo-area-message)
-(setq inhibit-startup-screen t)
+
+
+;; ==================================================================
+;; Editor settings
+;; ==================================================================
+
+;; Reload buffers automatically when they're changed on disk
+(setq global-auto-revert-mode t)
 
 ;; Fix default C/C++ formatting
 (setq c-default-style "linux"
       c-basic-offset 4)
+
+;; Load and use go-mode
+(load (expand-file-name "go-mode.el" user-emacs-directory))
+(require 'go-mode)
